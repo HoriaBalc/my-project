e86eea65d11474722dcb0f205f82312889a5b815
balc_horia@yahoo.com <balc_horia@yahoo.com>
2021-09-07 16:04:34 +0300

    about me section update

--- sendEmail.js
+++ sendEmail.js
@@ -1,28 +1,48 @@
-const send=document.querySelector("#send");
- let yourEmail=document.querySelector("#yourEmail");
-    let password=document.querySelector("#password");
-    let subject=document.querySelector("#subject");
-    let content=document.querySelector("#content");
-send.addEventListener('click',() => {
-    console.log(yourEmail.value);
-    console.log(password.value);
-    console.log(subject.value);
-    console.log(content.value);
-
-    sendEmail();
-});
-function sendEmail() {
-    Email.send({
-      Host: "smtp.gmail.com",
-      Username: yourEmail.value,
-      Password: password.value,
-      To: 'balc_horia@yahoo.com',
-      From: yourEmail.value,
-      Subject: subject.value,
-      Body: content.value,
+function deleteFields() {
+    setTimeout(function(){
+      let email=document.querySelector("#email");
+      let message=document.querySelector("#message");
+      if(message.value!=="" && email.value!==""){
+        message.value="";
+        email.value="";
+      }
       
-    })
-      .then(function (message) {
-        alert("mail sent successfully")
-      });
+    }, 1000);
+    
   }
+
+let page1= document.querySelector("#pag1");
+let page2= document.querySelector("#pag2");
+let prevPage=document.querySelector("#prev");
+let nextPage=document.querySelector("#next");
+let curentPage=1;
+//let page3= document.querySelector("#pag3");
+
+function prev(){
+  if(curentPage>1){
+    curentPage=-1;
+  }
+  if(curentPage==1){
+    prevPage.style.display="none";
+    nextPage.style.display="inline";
+    page2.style.display="none";
+    page1.style.display="block";
+  }
+}
+
+function next(){
+  if(curentPage>1){
+    curentPage=-1;
+  }
+  if(curentPage==1){
+    prevPage.style.display="inline";
+    nextPage.style.display="none";
+    page2.style.display="block";
+    page1.style.display="none";
+  }
+  
+}
+
+
+
+
